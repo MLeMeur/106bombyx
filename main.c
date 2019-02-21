a3266bab04e02ae0eb26aa361fedb598c105c7e6
LE MEUR Mathis <mathis@localhost.localdomain>
2019-02-21 07:48:23 -0500

    correction bad indentation

--- main.c
+++ main.c
@@ -12,48 +12,48 @@
 
 void part_normal(arg_t *arg)
 {
-		double x = arg->n;
+	double x = arg->n;
 
-		for (int i = 1; i <= 100; i++) {
-				printf("%d %.2f\n", i, x);
-				x = arg->rate * x * ((1000 - x) / 1000);
-		}
+	for (int i = 1; i <= 100; i++) {
+		printf("%d %.2f\n", i, x);
+		x = arg->rate * x * ((1000 - x) / 1000);
+	}
 }
 
 void part_generation(arg_t *arg)
 {
-		double k = 1;
-		double x = arg->n;
+	double k = 1;
+	double x = arg->n;
 
-		for (k = 1; k <= 4; k += 0.01) {
-				for (int i = 1; i <= arg->generation_final; i++) {
-						while (i < arg->generation_1) {
-								x = k * x * ((1000 - x) / 1000);
-								i++;
-						}
-						x = k * x * ((1000 - x) / 1000);
-						printf("%.2f %.2f\n", k, x);
-				}
+	for (k = 1; k <= 4; k += 0.01) {
+		for (int i = 1; i <= arg->generation_final; i++) {
+			while (i < arg->generation_1) {
+				x = k * x * ((1000 - x) / 1000);
+				i++;
+			}
+			x = k * x * ((1000 - x) / 1000);
+			printf("%.2f %.2f\n", k, x);
 		}
+	}
 }
 
 int main(int ac, char **av)
 {
-    arg_t *arg;
+  arg_t *arg;
 
-		if (ac == 2 && ((my_strlen(av[1]) == 1 && av[1][0] == 'h') ||
-				(my_strlen(av[1]) == 2 && av[1][1] == 'h'))) {
-				get_help();
-				return (SUCCESS);
-		}
-		if (check_error(ac, av) == FALSE)
-				return (FAIL);
-		arg = get_arg(ac, av);
-		if (arg->type == 1) {
-				part_normal(arg);
-		}
-		if (arg->type == 2) {
-				part_generation(arg);
-		}
-		return (TRUE);
+	if (ac == 2 && ((my_strlen(av[1]) == 1 && av[1][0] == 'h') ||
+		(my_strlen(av[1]) == 2 && av[1][1] == 'h'))) {
+			get_help();
+			return (SUCCESS);
+	}
+	if (check_error(ac, av) == FALSE)
+		return (FAIL);
+	arg = get_arg(ac, av);
+	if (arg->type == 1) {
+		part_normal(arg);
+	}
+	if (arg->type == 2) {
+		part_generation(arg);
+	}
+	return (TRUE);
 }
